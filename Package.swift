f86418b74cde4359b88c0e385f6a8049cc49409c
Jacob Martin <martinjaco@gmail.com>
2019-03-27 23:01:10 -0400

    update package

--- Package.swift
+++ Package.swift
@@ -1,15 +1,10 @@
-//
-//  Ciao.swift
-//  Ciao
-//
-//  Created by Alexandre Mantovani Tavares on 23/10/15.
-//  Copyright © 2017 AlTavares. All rights reserved.
-//
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
     name: "Ciao",
     dependencies: [],
-    exclude: ["Tests"]
+    exclude: ["Tests"],
+    swiftLanguageVersions: [.v4, .v4_2]
 )
